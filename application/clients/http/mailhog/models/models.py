bb193de5b7fb6f5c0fe7c55f10ce44b75e39b220
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2024-12-15 14:03:36 +0300

    Баг фикс получения писем

--- application/clients/http/mailhog/models/models.py
+++ application/clients/http/mailhog/models/models.py
@@ -6,54 +6,54 @@ from pydantic import BaseModel, Field
 
 
 class From(BaseModel):
-    relays: Any = Field(..., alias="Relays")
-    mailbox: str = Field(..., alias="Mailbox")
-    domain: str = Field(..., alias="Domain")
-    params: str = Field(..., alias="Params")
+    relays: Any = Field(None, alias="Relays")
+    mailbox: str = Field(None, alias="Mailbox")
+    domain: str = Field(None, alias="Domain")
+    params: str = Field(None, alias="Params")
 
 
 class ToItem(BaseModel):
-    relays: Any = Field(..., alias="Relays")
-    mailbox: str = Field(..., alias="Mailbox")
-    domain: str = Field(..., alias="Domain")
-    params: str = Field(..., alias="Params")
+    relays: Any = Field(None, alias="Relays")
+    mailbox: str = Field(None, alias="Mailbox")
+    domain: str = Field(None, alias="Domain")
+    params: str = Field(None, alias="Params")
 
 
 class Headers(BaseModel):
-    content__type: List[str] = Field(..., alias="Content-Type")
-    date: List[str] = Field(..., alias="Date")
-    from_: List[str] = Field(..., alias="From")
-    mime__version: List[str] = Field(..., alias="MIME-Version")
-    message__id: List[str] = Field(..., alias="Message-Id")
-    received: List[str] = Field(..., alias="Received")
-    reply__to: List[str] = Field(..., alias="Reply-To")
-    return__path: List[str] = Field(..., alias="Return-Path")
-    subject: List[str] = Field(..., alias="Subject")
-    to: List[str] = Field(..., alias="To")
+    content__type: List[str] = Field(None, alias="Content-Type")
+    date: List[str] = Field(None, alias="Date")
+    from_: List[str] = Field(None, alias="From")
+    mime__version: List[str] = Field(None, alias="MIME-Version")
+    message__id: List[str] = Field(None, alias="Message-Id")
+    received: List[str] = Field(None, alias="Received")
+    reply__to: List[str] = Field(None, alias="Reply-To")
+    return__path: List[str] = Field(None, alias="Return-Path")
+    subject: List[str] = Field(None, alias="Subject")
+    to: List[str] = Field(None, alias="To")
 
 
 class Content(BaseModel):
-    headers: Headers = Field(..., alias="Headers")
-    body: str = Field(..., alias="Body")
-    size: int = Field(..., alias="Size")
-    mime: Any = Field(..., alias="MIME")
+    headers: Headers = Field(None, alias="Headers")
+    body: str = Field(None, alias="Body")
+    size: int = Field(None, alias="Size")
+    mime: Any = Field(None, alias="MIME")
 
 
 class Raw(BaseModel):
-    from_: str = Field(..., alias="From")
-    to: List[str] = Field(..., alias="To")
-    data: str = Field(..., alias="Data")
-    helo: str = Field(..., alias="Helo")
+    from_: str = Field(None, alias="From")
+    to: List[str] = Field(None, alias="To")
+    data: str = Field(None, alias="Data")
+    helo: str = Field(None, alias="Helo")
 
 
 class Item(BaseModel):
-    id: str = Field(..., alias="ID")
-    from_: From = Field(..., alias="From")
-    to: List[ToItem] = Field(..., alias="To")
-    content: Content = Field(..., alias="Content")
-    created: str = Field(..., alias="Created")
-    mime: Any = Field(..., alias="MIME")
-    raw: Raw = Field(..., alias="Raw")
+    id: str = Field(None, alias="ID")
+    from_: From = Field(None, alias="From")
+    to: List[ToItem] = Field(None, alias="To")
+    content: Content = Field(None, alias="Content")
+    created: str = Field(None, alias="Created")
+    mime: Any = Field(None, alias="MIME")
+    raw: Raw = Field(None, alias="Raw")
 
 
 class Messages(BaseModel):
