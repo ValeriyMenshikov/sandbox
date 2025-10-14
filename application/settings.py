b0a530f2d052cd2fbfd53972e5f7932d18686baa
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2025-10-14 15:39:04 +0300

    сменил хост

--- application/settings.py
+++ application/settings.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings
 
+# BASE_HOST = "5.63.153.31"
+BASE_HOST = "185.185.143.231"
 
 class Settings(BaseSettings):
-    HTTP_API_ACCOUNT: str = "http://5.63.153.31:5051/"
-    HTTP_API_LOGIN: str = "http://5.63.153.31:5051/"
-    HTTP_API_FORUM: str = "http://5.63.153.31:5051/"
-    HTTP_MAILHOG: str = "http://5.63.153.31:5025/"
+    HTTP_API_ACCOUNT: str = f"http://{BASE_HOST}:5051/"
+    HTTP_API_LOGIN: str = f"http://{BASE_HOST}:5051/"
+    HTTP_API_FORUM: str = f"http://{BASE_HOST}:5051/"
+    HTTP_MAILHOG: str = f"http://{BASE_HOST}:5025/"
     DISABLE_LOG: bool = False
     # DB_HOST: str = "postgres"
-    DB_HOST: str = "5.63.153.31"
+    DB_HOST: str = BASE_HOST
     DB_PORT: int = 5432
     DB_USER: str = "postgres"
     DB_PASSWORD: str = "admin"
@@ -26,11 +28,11 @@ class Settings(BaseSettings):
     CH_USER: str = ""
     CH_PASSWORD: str = ""
     MAIL_FROM: str = '"DM.am" <info@dm.am>'
-    SMTP_HOST: str = "5.63.153.31"
+    SMTP_HOST: str = BASE_HOST
     SMTP_PORT: int = 1025
     SMTP_PASSWORD: str = ""
     # KAFKA_URL: str = "kafka:19092"
-    KAFKA_URL: str = "5.63.153.31:9092"
+    KAFKA_URL: str = f"{BASE_HOST}:9092"
     # KAFKA_URL: str = "localhost:9092"
     KAFKA_REGISTER_TOPIC: str = "register-events"
     KAFKA_REGISTER_TOPIC_ERROR: str = "register-events-errors"
